dee9450db2b43d7c53016810dd0e9374ffc63093
Margaret-Ann Seger <mseger09@gmail.com>
2014-05-29 09:39:39 -0700

    Removed test files to tighten up codebase

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
 var main = require('./routes/main');
-var testEndpoint = require('./routes/testEndpoint');
 var http = require('http');
 var path = require('path');
 
@@ -32,7 +31,6 @@ if ('development' == app.get('env')) {
 
 app.get('/', main.display);
 app.get('/users', user.list);
-app.get('/test', testEndpoint.display);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/testEndpoint.js
+++
@@ -1,8 +0,0 @@
-
-/*
- * RENDER test jade file  
- */
-
-exports.display = function(req, res){
-  res.render('bigpicture', { title: 'Test Endpoint' });
-};
